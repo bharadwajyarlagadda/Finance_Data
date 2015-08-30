f0efb3f95a42b6c4cc3a56db7f318341cca49d2d
unknown <yarlagaddabharadwaj@gmail.com>
2015-08-29 23:20:00 -0400

    getting summary and calls completed

--- Yahoo/main.py
+++ Yahoo/main.py
@@ -4,16 +4,22 @@ from Yahoo import url_construct
 from Yahoo import stock_symbol_summary
 from Yahoo import create_csv
 from Yahoo import calls_and_puts_url_construct
+from Yahoo import get_calls
 
 url = "http://finance.yahoo.com/"
 
 yahoo_Url_Construct = url_construct.yahoo_Url_Construct(url)
 stock_symbols_summary_urls = yahoo_Url_Construct.execute()
-print(stock_symbols_summary_urls)
 for key, values in stock_symbols_summary_urls.items():
     calls_and_puts_url = calls_and_puts_url_construct.calls_and_puts_url_construct\
         (key, stock_symbols_summary_urls[key], url)
-    calls_and_puts_url.execute()
+    stock_sym, options_url = calls_and_puts_url.execute()
+    if options_url:
+        print("Retrieved options_url: " + options_url)
+        retrieve_get_calls = get_calls.get_calls(stock_sym, options_url)
+        retrieve_get_calls.execute()
+    else:
+        print(stock_sym + ": This Stock symbol doesn't have any calls or puts today")
     break
 
 #for key, values in stock_symbols_summary_urls.items():
